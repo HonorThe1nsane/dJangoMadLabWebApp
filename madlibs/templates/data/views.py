5ad384492b5d7d1350a57bff91742df072721737
HonorThe1nsane <troymd2009@hotmail.com>
2023-12-09 14:31:32 -0700

    restructured

--- madlibs/templates/data/views.py
+++ madlibs/templates/data/views.py
@@ -1,6 +1,6 @@
 # madlibs/views.py
 from django.shortcuts import render
-from .models import MadLib  # Import your model
+from ....models import MadLib  # Import your model
 
 def madlib_list(request):
     madlibs = MadLib.objects.all()  # Replace with your logic to fetch Mad Libs
@@ -9,3 +9,7 @@ def madlib_list(request):
 def madlib_detail(request, madlib_id):
     madlib = MadLib.objects.get(id=madlib_id)  # Replace with your logic to fetch a specific Mad Lib
     return render(request, 'data/madlib_detail.html', {'madlib': madlib})
+
+def hello_view(request):
+    today = datetime.datetime.now().date()
+    return render(request, 'data/hello.html', '{"today" : today}')
--- madlibs/urls.py
+++ madlibs/urls.py
@@ -1,9 +1,10 @@
 # madlibs/urls.py
 from django.urls import path
-from . import views
+from .templates.data import views
 
 urlpatterns = [
     path('', views.madlib_list, name='madlib_list'),
     path('<int:madlib_id>/', views.madlib_detail, name='madlib_detail'),
+    path('hello/', hello_view, name='hello_view')
     # Add other URLs as needed
 ]
